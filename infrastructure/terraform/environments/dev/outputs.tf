7c3eb67eef77d027c9d9c518e8e5933d24a996e4
Baroni Pierre <pierre.baroni@free.fr>
2025-12-04 15:32:52 +0100

    Add GitHub Actions deployment workflow

---
+++ infrastructure/terraform/environments/dev/outputs.tf
@@ -0,0 +1,11 @@
+output "acr_name" {
+  value = module.acr.acr_name
+}
+
+output "aks_name" {
+  value = module.aks.aks_name
+}
+
+output "resource_group_name" {
+  value = module.resource_group.resource_group_name
+}
